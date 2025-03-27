e637a612a74d91bcd57f9dea631056369b30b3b5
mansi1788 <maithanimansi8@gmail.com>
2025-03-27 22:09:30 +0530

    "render update"

--- frontend/src/api/PostRequest.js
+++ frontend/src/api/PostRequest.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'http://https://social-media2-590u.onrender.com:5000' });
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
